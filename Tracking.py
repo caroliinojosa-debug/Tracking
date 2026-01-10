78468732e889698bf71d3cd59d12c5d939aac638
Carolinita007 <caroli.inojosa@gmail.com>
2026-01-10 17:45:30 -0400

    Update Tracking.py

--- Tracking.py
+++ Tracking.py
@@ -5,8 +5,7 @@ import os
 import smtplib
 from email.message import EmailMessage
 from fastapi import FastAPI
-import flet_fastapi
- 
+import flet_fastapi 
 import json
 
 # --- CONFIGURACIÓN DE GOOGLE SHEETS ---
@@ -20,8 +19,10 @@ MI_PASSWORD="axojrteyadfhqofs"
 CORREO_VENTAS="rhextrufan@gmail.com"
 
 def conectar_hoja():
-    creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, SCOPE)
-    creds_dict = json.loads(os.environ.get("GOOGLE_CREDS"))
+    import json
+    # Esto evita que la app explote al no encontrar el archivo .json
+    creds_json = os.environ.get("GOOGLE_CREDS")
+    creds_dict = json.loads(creds_json)
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPE)
     client = gspread.authorize(creds)
     return client.open(SHEET_NAME).sheet1
@@ -202,13 +203,7 @@ async def main(page: ft.Page):
 
 app = FastAPI()
 
-# Tu función corregida
-async def main(page: ft.Page):
-    # Todo tu código de la app aquí...
-    page.add(ft.Text("App funcionando"))
-
-
-# La línea que ya tienes
 app.mount("/", flet_fastapi.app(main, assets_dir="assets"))
+
 
 
