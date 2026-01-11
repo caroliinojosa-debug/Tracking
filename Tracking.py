790655ccb8f7a5d68b06314e181fa8531e197f4c
Carolinita007 <caroli.inojosa@gmail.com>
2026-01-10 20:37:40 -0400

    Update Tracking.py

--- Tracking.py
+++ Tracking.py
@@ -272,11 +272,14 @@ async def main(page: ft.Page):
 # La llamada inicial al final de tu def main() también cambia:
     await mostrar_menu_principal()
 
-if __name__ == "__main__":
-    # Esto permite que Railway/Uvicorn reconozca la app correctamente
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 app.mount("/", flet_fastapi.app(main, assets_dir="assets"))
+
+# Esto es para que Railway sepa qué puerto usar
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 
