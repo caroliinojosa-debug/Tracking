74bd1df7463a939d3559791088580289e3a1c444
Carolinita007 <caroli.inojosa@gmail.com>
2026-01-13 16:17:48 -0400

    Update Tracking.py

--- Tracking.py
+++ Tracking.py
@@ -168,11 +168,11 @@ async def main(page: ft.Page):
                         est = {c.label: c.value for c in checks}
                         nuevos.append({"id": txt_id.value, "estados": est})
                         try: enviar_aviso_ventas(txt_id.value, est)
-                           except Exception as e: 
-                               print("------------------------------------------")
-                               print(f"⚠️ ATENCIÓN: El correo no se envió.")
-                               print(f"Detalle técnico: {e}")
-                               print("------------------------------------------")
+                        except Exception as e: 
+                            print("------------------------------------------")
+                            print(f"⚠️ ATENCIÓN: El correo no se envió.")
+                            print(f"Detalle técnico: {e}")
+                            print("------------------------------------------")
                     guardar_en_sheets(nuevos)
                 
                 await asyncio.to_thread(tarea_backend)
@@ -247,6 +247,7 @@ app.mount("/", app_flet)
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("Tracking:app", host="0.0.0.0", port=port, reload=False)
+
 
 
 
